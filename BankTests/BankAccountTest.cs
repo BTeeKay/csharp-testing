acad87a51c36cd9198626a4035a89f1f5f0a63f4
SkyeEli <briantkerr91@gmail.com>
2022-09-12 15:58:01 +0100

    Added more tests

--- BankTests/BankAccountTest.cs
+++ BankTests/BankAccountTest.cs
@@ -40,5 +40,28 @@ namespace BankTests
             Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debiamount));
         }
 
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 20.0;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
+        }
+
     }
 }
